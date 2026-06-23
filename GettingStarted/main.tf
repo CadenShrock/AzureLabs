c82c30b90ac09f58c5b1a7d773e4dba887981f43
Caden Shrock <cadshro@gmail.com>
2026-06-23 12:31:13 -0400

    added tags to TFResourceGroup and added a VNET  and to main.tf in GettingStarted

--- GettingStarted/main.tf
+++ GettingStarted/main.tf
@@ -17,6 +17,10 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
 }
 
 # Create a virtual network
